e70deaf0a84a6dbeec5e3c1f038f585329542aa3
shlihs95 <fhzptvh@gmail.com>
2025-08-11 01:51:43 +0900

    chore: 도커 테스트 컨테이너 실행을 위한 설정 파일 수정

--- src/main/java/kr/hhplus/be/server/ServerApplication.java
+++ src/main/java/kr/hhplus/be/server/ServerApplication.java
@@ -2,8 +2,12 @@ package kr.hhplus.be.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.project")
+@EnableJpaRepositories(basePackages = "com.project.infrastructure")
+@EntityScan("com.project.domain.entity")
 public class ServerApplication {
 
 	public static void main(String[] args) {
